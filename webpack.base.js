69726d3ed0efd6a6b41834ea51aa3c96c4692465
xiongying <648051498@qq.com>
2020-03-03 23:43:46 +0800

    区分环境配置打包文件

---
+++ webpack.base.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  module: {
+      rules: [
+        { 
+          test: /\.js$/, 
+          exclude: /node_modules/,  
+          loader: "babel-loader", // es6与es5通信桥梁
+        }, 
+        // {
+        //   test: /\.jpg$/,
+        //   use: {
+        //     loader: 'file-loader',
+        //     options: {
+        //       name: '[name]_[hash].[ext]', // [name]、[hash]、[ext]占位符（placeholder）
+        //       outputPath: 'images/' // 打包输出目录
+        //     }
+        //   }
+        // },
+        {
+          test: /\.(jpg|png|gif)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              name: '[name]_[hash].[ext]',
+              outputPath: 'images/', 
+              limit: 2048 // 大于2048kb的图片不打包到js文件中
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'] // loader的执行顺序：从下到上、从右至左
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2, // 通过import引入的scss文件也要执行下面的两个loader
+              modules: true // 开启css模块化打包，减少css文件的耦合性
+            }
+          }, 'postcss-loader', 'sass-loader'] 
+        },
+        {
+          test: /\.(eot|svg|ttf|woff)/,
+          use: 'file-loader'
+        }
+      ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/template.html'
+    }), // 在打包结束后自动生成index.html，并把打包生成的js文件自动引入其中
+    new CleanWebpackPlugin(), // 自动清空dist
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,22 @@
 const path = require('path') // node核心模块
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
 
-module.exports = {
+const devConfig = {
   mode: 'development', // 默认是production
   devtool: 'cheap-module-eval-source-map', // 设置了开发模式，会自动使用source-map模式
-  entry: {
-    index: './src/index.js',
-    // sub: './src/index.js'
-  },
   output: {
     // publicPath: 'http://cdn.com.cn', // 配置打包输出公共路径
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist') // 绝对路径，默认文件夹dist
   },
-  module: {
-      rules: [
-        { 
-          test: /\.js$/, 
-          exclude: /node_modules/,  
-          loader: "babel-loader", // es6与es5通信桥梁
-        }, 
-        // {
-        //   test: /\.jpg$/,
-        //   use: {
-        //     loader: 'file-loader',
-        //     options: {
-        //       name: '[name]_[hash].[ext]', // [name]、[hash]、[ext]占位符（placeholder）
-        //       outputPath: 'images/' // 打包输出目录
-        //     }
-        //   }
-        // },
-        {
-          test: /\.(jpg|png|gif)$/,
-          use: {
-            loader: 'url-loader',
-            options: {
-              name: '[name]_[hash].[ext]',
-              outputPath: 'images/', 
-              limit: 2048 // 大于2048kb的图片不打包到js文件中
-            }
-          }
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'] // loader的执行顺序：从下到上、从右至左
-        },
-        {
-          test: /\.scss$/,
-          use: ['style-loader', {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2, // 通过import引入的scss文件也要执行下面的两个loader
-              modules: true // 开启css模块化打包，减少css文件的耦合性
-            }
-          }, 'postcss-loader', 'sass-loader'] 
-        },
-        {
-          test: /\.(eot|svg|ttf|woff)/,
-          use: 'file-loader'
-        }
-      ]
-  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/template.html'
-    }), // 在打包结束后自动生成index.html，并把打包生成的js文件自动引入其中
-    new CleanWebpackPlugin(), // 自动清空dist
     new webpack.HotModuleReplacementPlugin() // 引用webpack自带的模块热更新插件
   ],
+  optimization: {
+    usedExports: true // 开启tree sharking
+  },
   devServer: { // 开启服务
     contentBase: './dist',
     open: true, // 自动打开浏览器
@@ -81,3 +28,5 @@ module.exports = {
     hotOnly: true // 可不设置，在某些模块不支持热更新的情况下，不刷新页面，而是在控制台输出热更新失败
   }
 }
+
+module.exports = merge(baseConfig, devConfig)
